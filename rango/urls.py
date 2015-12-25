272c654941324ad620a355992db5e8df3ab5c974
naman007 <7886636@qq.com>
2015-12-25 07:47:44 +0000

    add form DEMO

--- rango/urls.py
+++ rango/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = [
-    url(r'^$', views.inex, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^about', views.about, name ='about'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+    return render(request, 'rango/add_category.html',{'form':form})
+
+
 
 
 def category(request, category_name_slug):
@@ -23,7 +39,7 @@ def category(request, category_name_slug):
 
 
 
-def inex(request):
+def index(request):
     category_list= Category.objects.order_by('-likes')[:5]
     context_dict ={'categorys':category_list}
     return render(request, 'rango/index.html',context_dict)
