c8e2b53c5e1354748beea39ac3d28a8f5695d080
naman007 <7886636@qq.com>
2015-12-28 12:45:51 +0000

    add register model

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
+
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
@@ -10,6 +12,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page,PageAdmin)
+admin.site.register(UserProfile)
 
 
 # Register your models here.
--- rango/models.py
+++ rango/models.py
@@ -1,6 +1,17 @@
 from __future__ import unicode_literals
 from django.template.defaultfilters import slugify
 from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __unicode__(self):
+        return self.user.username
+
+
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
